c28698abd05c4f3de9fe21f422410d1c1f312a1d
vit100 <vit100@yahoo.com>
2016-03-27 12:45:44 -0400

    DifferentSchema - each item in number has child translation

--- EntityFrameworkGames/Program.cs
+++ EntityFrameworkGames/Program.cs
@@ -6,28 +6,24 @@ using System.Linq;
 
 namespace EntityFrameworkGames
 {
-    class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
             using (var ctx = new MyContext())
             {
-                var translations = ctx.Translations.Where(t => t.TableName == "Number");
                 var q = from n in ctx.Numbers
-                        join translation in translations on n.Id equals translation.NumberId into trGr
-                        from t in trGr.DefaultIfEmpty()
                         orderby n.Order
                         select new
                         {
-                            name = n.Name,
-                            translation = t == null ? n.Name : t.Text,
-                            table = t.TableName
+                            value = n.Name,
+                            translation = n.Translation.Text
                         };
                 foreach (var item in q)
                 {
-                    Console.WriteLine(item.name + " -> " + item.translation);
+                    Console.WriteLine(item.value + "->" + item.translation);
                 }
-                Console.Read();
+                Console.ReadKey();
             }
         }
 
@@ -40,11 +36,16 @@ namespace EntityFrameworkGames
                 modelBuilder.Entity<Number>()
                     .HasKey(p => p.Id)
                     .Property(p => p.Id)
+                    .HasColumnOrder(0)
                     .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
+                modelBuilder.Entity<Number>()
+                    .Property(p => p.Order)
+                    .HasColumnOrder(1);
 
                 modelBuilder.Entity<Translation>()
                     .HasKey(p => p.Id)
                     .Property(p => p.Id)
+                    .HasColumnOrder(0)
                     .HasDatabaseGeneratedOption(DatabaseGeneratedOption.Identity);
             }
 
@@ -54,43 +55,53 @@ namespace EntityFrameworkGames
             }
 
             public DbSet<Number> Numbers { get; set; }
-            public DbSet<Translation> Translations { get; set; }
+            public DbSet<Word> Words { get; set; }
         }
 
         class myConf : DropCreateDatabaseIfModelChanges<MyContext>
         {
             protected override void Seed(MyContext context)
             {
-                var numbers = new List<Number> { new Number { Name = "1", Order = 3 }, new Number { Name = "2", Order = 2 }, new Number { Name = "3", Order = 1 } };
-                var translations = new List<Translation>
+                var numbers = new List<Number> {
+                    new Number { Name = "1", Order = 3, Translation = new Translation { Text = "one" }},
+                    new Number { Name = "2", Order = 2, Translation = new Translation {Text = "two"} },
+                    new Number { Name = "3", Order = 1 }};
+
+                var words = new List<Word>
                 {
-                    new Translation {NumberId = 1, TableName = "Number", Text = "one"},
-                    new Translation {NumberId = 2, TableName = "Number", Text = "two"}
+                    new Word {Name="car", Order = 1,Translation = new Translation {Text = "eng:car"} },
+                    new Word {Name="bike", Order = 2,Translation = new Translation {Text = "eng:bike"} }
                 };
                 context.Numbers.AddRange(numbers);
-                context.Translations.AddRange(translations);
+                context.Words.AddRange(words);
                 base.Seed(context);
 
             }
         }
         #endregion
 
-
-        public class Number
+        public abstract class OrderedItem
         {
-            [Column(Order = 1)]
+            public int Order { get; set; }
+        }
+
+        public class Number : OrderedItem
+        {
             public int Id { get; set; }
             public string Name { get; set; }
+            public Translation Translation { get; set; }
+        }
 
-            [Column(Order = 2)]
-            public int Order { get; set; }
+        public class Word : OrderedItem
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public Translation Translation { get; set; }
         }
 
         public class Translation
         {
             public int Id { get; set; }
-            public int NumberId { get; set; }
-            public string TableName { get; set; }
             public string Text { get; set; }
         }
 
